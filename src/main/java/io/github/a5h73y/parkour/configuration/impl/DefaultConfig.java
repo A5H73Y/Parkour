35396a5867c3f3731ec2a340be45a3cb4d0c6ec0
Guillaume (URoulon) <uroulon@gmail.com>
2020-09-21 18:09:59 +0200

    Changed the configuration option's name to "Other.Display.OnlyReadyCourses" and changed its default value to false to maintain current default behaviour

--- src/main/java/io/github/a5h73y/parkour/configuration/impl/DefaultConfig.java
+++ src/main/java/io/github/a5h73y/parkour/configuration/impl/DefaultConfig.java
@@ -135,7 +135,7 @@ public class DefaultConfig extends ParkourConfiguration {
 		this.addDefault("Other.UseSounds", true);
 		this.addDefault("Other.EnforceSafeCheckpoints", true);
 		this.addDefault("Other.UseAutoTabCompletion", true);
-		this.addDefault("Other.ShowUnfinishedCoursesToGuests", true);
+		this.addDefault("Other.Display.OnlyReadyCourses", false);
 		this.addDefault("Other.ParkourKit.ReplaceInventory", true);
 		this.addDefault("Other.ParkourKit.GiveSign", true);
 		this.addDefault("Other.Parkour.ChatRankPrefix.Enabled", false);
